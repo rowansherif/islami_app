5e20c13382d9699f5ce4924d35bd5e8a3c7306bf
rowansherif <rsherif1971@gmail.com>
2024-12-15 03:51:18 +0200

    Added sebha screen UI.

--- lib/home/taps/sebha/sebha_screen.dart
+++ lib/home/taps/sebha/sebha_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app_practice/app_colors.dart';
 
 class SebhaScreen extends StatelessWidget {
-  const SebhaScreen({super.key});
+  int counter = 0;
+  List <String> tsabeh = [
+    'سبحان الله',
+    'الحمد لله',
+    'الله اكبر'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/islami_logo.png'),
+          Text('سَبِّحِ اسْمَ رَبِّكَ الأعلى ', style: 
+            TextStyle(color: AppColors.whiteColor,
+            fontSize: 36),),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset('assets/images/sebha_image.png'),
+              Container(
+                margin: EdgeInsets.only(top: 70),
+                child: Column(
+                  children: [
+                    Text('سبحان الله', 
+                    style: 
+                    TextStyle(color: AppColors.whiteColor,
+                    fontSize: 36),),
+                    Text('$counter', style: 
+                    TextStyle(color: AppColors.whiteColor,
+                    fontSize: 36),),
+                      
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
